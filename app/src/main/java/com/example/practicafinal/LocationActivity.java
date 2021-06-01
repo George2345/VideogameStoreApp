9e49b8eacbfe26244897663fcf50cfe3c975a251
xXaVeRaXx <avdepaul11@gmail.com>
2021-06-01 22:32:18 +0200

    Intento de Mapa

--- app/src/main/java/com/example/practicafinal/LocationActivity.java
+++ app/src/main/java/com/example/practicafinal/LocationActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LocationActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady (GoogleMap googleMap){
+        googleMap.addMarker(new MarkerOptions()
+            .position(new LatLng(0,0))
+            .title("Marker")
+        );
     }
 }
