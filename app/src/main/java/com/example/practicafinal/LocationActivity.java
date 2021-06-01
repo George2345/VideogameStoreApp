9f9c76f9006e86b3bc70f3341fcad714be5bc0f0
xXaVeRaXx <avdepaul11@gmail.com>
2021-06-01 22:47:52 +0200

    Actividad Location "completada"

--- app/src/main/java/com/example/practicafinal/LocationActivity.java
+++ app/src/main/java/com/example/practicafinal/LocationActivity.java
@@ -1,21 +1,49 @@
 package com.example.practicafinal;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.navigation.NavigationView;
 
-public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback, NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+
+        //Toolbar
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                toolbar,
+                0,
+                0
+        );
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationdrawer_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -28,5 +56,86 @@ public class LocationActivity extends AppCompatActivity implements OnMapReadyCal
             .position(new LatLng(0,0))
             .title("Marker")
         );
+    }
+
+    //Icono del menú lateral izquierdo
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+
+    }
+
+    //Icono de la derecha carrito
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //Darle funcionalidad al icono del carrito
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.marketCart:
+                Intent intent = new Intent(this, ShoppingCartActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return false;
+    }
+
+    //funcionalidad menú lateral izquierdo
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Intent intent = new Intent(this, SectionActivity.class);
+        switch (id)
+        {
+            case R.id.navigation_home:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.navigation_novedades:
+                intent.putExtra("SECTION_ID", 0);
+                startActivity(intent);
+                break;
+            case R.id.navigation_ofertas:
+                intent.putExtra("SECTION_ID", 1);
+                startActivity(intent);
+                break;
+            case R.id.navigation_ps4:
+                intent.putExtra("SECTION_ID", 2);
+                startActivity(intent);
+                break;
+            case R.id.navigation_xbox:
+                intent.putExtra("SECTION_ID", 3);
+                startActivity(intent);
+                break;
+            case R.id.navigation_carrito:
+                Intent intentShopCart = new Intent(this, ShoppingCartActivity.class);
+                startActivity(intentShopCart);
+                break;
+            case R.id.navigation_location:
+                Intent intentLocation = new Intent(this, LocationActivity.class);
+                startActivity(intentLocation);
+                break;
+            case R.id.navigation_contact:
+                Intent intent_contact = new Intent(this, ContactActivity.class);
+                startActivity(intent_contact);
+                break;
+        }
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
